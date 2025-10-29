d7d187c565bc3da45a9a7cf47e78776ea4a27cad
MD SAYDUL ISLAM <islammdsaydul295@gmail.com>
2025-10-29 17:57:07 +0100

    Uncomment scrollify initialization in main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,3 @@
-var mixer=mixitup('.grid-portfolio-container');/*$(function(){$.scrollify({section:".scrollify",setHeights:!1,updateHash:!1,scrollSpeed:800,})});*/ function openMenu(){document.getElementById('navbar').style.height="100%";document.getElementById('menuIcon').style.visibility="hidden"}
+var mixer=mixitup('.grid-portfolio-container');$(function(){$.scrollify({section:".scrollify",setHeights:!1,updateHash:!1,scrollSpeed:800,})});function openMenu(){document.getElementById('navbar').style.height="100%";document.getElementById('menuIcon').style.visibility="hidden"}
 function closeMenu(){document.getElementById('navbar').style.height="0%";document.getElementById('menuIcon').style.visibility="visible"}
 $(function(){$('.menu-items a, .scroll-down a').on('click',function(){$('html, body').animate({scrollTop:$($.attr(this,'href')).offset().top},1000);return!1})})
